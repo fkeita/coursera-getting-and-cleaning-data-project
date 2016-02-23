51d0666df42ce4e8b7e3ad23dc91799743093200
fkeita <fousseni@moduscreate.com>
2016-02-22 20:03:33 -0500

    removed comment

--- project/run_analysis.R
+++ project/run_analysis.R
@@ -1,16 +1,16 @@
-#install.packages("reshape2")
-#library(reshape2)
+install.packages("reshape2")
+library(reshape2)
 
-#filename <- "getdata_dataset.zip"
+filename <- "getdata_dataset.zip"
 
 ## Download and unzip the dataset:
-#if (!file.exists(filename)){
-#  fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip "
-#  download.file(fileURL, filename, method="curl")
-#}  
-#if (!file.exists("UCI HAR Dataset")) { 
-#  unzip(filename) 
-#}
+if (!file.exists(filename)){
+  fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip "
+  download.file(fileURL, filename, method="curl")
+}  
+if (!file.exists("UCI HAR Dataset")) { 
+  unzip(filename) 
+}
 
 # Load activity labels + features
 activityLabels <- read.table("UCI HAR Dataset/activity_labels.txt")
